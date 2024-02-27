55a82b6e17562199f3c7a69dc536802bec3956a9
Akapaya <63330951+Akapaya@users.noreply.github.com>
2024-02-27 16:28:23 -0300

    Project Commit and Player Movements
    Project Commit and Player Movements

---
+++ Player.gd
@@ -0,0 +1,13 @@
+extends CharacterBody2D
+
+@export var moveVelocity = 600
+
+func _physics_process(delta):
+	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	velocity = direction * moveVelocity
+	move_and_slide()
+	
+	if(velocity.length() > 0.0):
+		get_node("HappyBoo").play_walk_animation()
+	else:
+		get_node("HappyBoo").play_idle_animation()	
