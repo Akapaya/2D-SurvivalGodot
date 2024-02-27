512885676991bc3192edf3c18c3560ab5ff84996
Akapaya <63330951+Akapaya@users.noreply.github.com>
2024-02-27 17:57:40 -0300

    Slime Take Damage and play animation
    Slime Take Damage and play animation

--- Fabs/Mobs/Slime1.gd
+++ Fabs/Mobs/Slime1.gd
@@ -1,13 +1,24 @@
 extends CharacterBody2D
 
 @export var moveVelocity = 300
+@export var health = 5
 
 var playerTarget
 
 func _ready():
 	playerTarget = get_node("/root/Game/Player")
+	%Slime.play_walk()
 
 func _physics_process(delta):
 	var direction = global_position.direction_to(playerTarget.global_position)
 	velocity = direction * moveVelocity
 	move_and_slide()
+
+
+func take_damage():
+	health-= 1
+	
+	%Slime.play_hurt()
+	
+	if health <= 0:
+		queue_free()
--- Player.gd
+++ Player.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 @export var moveVelocity = 600
 
+
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * moveVelocity
