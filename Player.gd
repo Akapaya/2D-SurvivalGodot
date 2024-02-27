f11c295d4b7ff65553f05a9714a6e0aa4aff7f55
Akapaya <63330951+Akapaya@users.noreply.github.com>
2024-02-27 18:09:05 -0300

    Added Health to player and damaged method
    Added Health to player and damaged method

--- Player.gd
+++ Player.gd
@@ -1,7 +1,10 @@
 extends CharacterBody2D
 
-@export var moveVelocity = 600
+signal health_depleted
 
+@export var moveVelocity = 600
+@export var health = 100.0
+@export var damage_rate = 5.0
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left", "move_right", "move_up", "move_down")
@@ -12,3 +15,9 @@ func _physics_process(delta):
 		%HappyBoo.play_walk_animation()
 	else:
 		%HappyBoo.play_idle_animation()	
+		
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= damage_rate * overlapping_mobs.size() * delta
+		if health <= 0.0:
+			health_depleted.emit()
